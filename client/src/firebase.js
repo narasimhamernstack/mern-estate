c0139be94146c26ebe575eafb68310a14fd2e08f
chinnanarasimharao <chinnachinna8548@gmail.com>
2024-01-07 15:47:46 +0530

    create and add OAuth component

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRHU4ehb2NlXffKp2Rz37bujxTaGWOEgc",
+  authDomain: "mern-estate-9ec91.firebaseapp.com",
+  projectId: "mern-estate-9ec91",
+  storageBucket: "mern-estate-9ec91.appspot.com",
+  messagingSenderId: "479291937692",
+  appId: "1:479291937692:web:b1c3d7ccdc20690aafae59"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
