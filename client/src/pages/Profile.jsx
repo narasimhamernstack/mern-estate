16a66c1d00cdd2025660af80434d7a1c92d7bacc
chinnanarasimharao <chinnachinna8548@gmail.com>
2024-05-21 07:37:46 +0530

    create user update api

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useState } from 'react-redux'
 
 export default function Profile() {
+  const [formData, setFormData] = useState({});
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
